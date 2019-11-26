40eeb05f95280be0b9d89d181f2b46e4a6735449
Victor Chung <victochung998@yahoo.com>
2019-11-25 19:06:02 -0800

    Added heroku postbuild scripts

---
+++ restaurant-menu(ByVictor)/build/precache-manifest.f6d9f081b4a2459fee22eb445108c33b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dcd16f256e01dc80a56abda5e0d945",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "0053acd212eaacf3ab45",
+    "url": "/restaurant-menu/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3450de7cb278ed36528f",
+    "url": "/restaurant-menu/static/js/2.e9ce073d.chunk.js"
+  },
+  {
+    "revision": "0053acd212eaacf3ab45",
+    "url": "/restaurant-menu/static/js/main.79a7dab2.chunk.js"
+  },
+  {
+    "revision": "2b58acf1d775f62181ce",
+    "url": "/restaurant-menu/static/js/runtime-main.f4e4870d.js"
+  }
+]);
