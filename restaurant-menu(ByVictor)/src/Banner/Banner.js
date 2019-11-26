ad0e9317f063c3a640e6f57461f42b5aacd41bec
Victor Chung <victochung998@yahoo.com>
2019-11-25 20:22:35 -0800

    added files -functional @ 2022 11.25.19

---
+++ restaurant-menu(ByVictor)/src/Banner/Banner.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+   height: 30px;
+   width: 100%;
+   /* background-image: url("/img/hornbill-flying.jpg"); */
+   background-position: center;
+   background-size: cover;
+   filter: contrast(75%);    
+    `
