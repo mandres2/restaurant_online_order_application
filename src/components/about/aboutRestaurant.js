a029d56751186ca4f309b86209065be54bf6206e
Micah Andres <mandres1@uw.edu>
2019-12-02 16:51:57 -0800

    Added details to about

--- src/components/about/aboutRestaurant.js
+++ src/components/about/aboutRestaurant.js
@@ -7,6 +7,8 @@ function AboutRestaurant() {
 
       <h1>About</h1>
 
+      <h3>Section where the customer can view restaurant information. (*Nice to have: Owner can enter and update About Section, anytime they log in to the application. Updates section real-time)</h3>
+
     </div>
   );
 }
